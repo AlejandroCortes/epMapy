0893a88f80dfa42c99ad6b1ce2004aa1641e8324
Edgar Alejandro Cortes-Calderon <eacortesc@gmail.com>
2024-06-26 15:40:04 +0000

    done

--- epMapy.py
+++ epMapy.py
@@ -1,45 +1,3 @@
-from tkinter import *
-import tkinter as tk
-from tkinter.ttk import *
-from tkinter import LabelFrame, Label, Tk#, Canvas
-from tkinter.ttk import Notebook
-import tkinter.messagebox
-import time
-import os
-
-
-import warnings
-warnings.filterwarnings('ignore')
-
-root = tk.Tk()
-
-class GUIDesign():
-    def __init__(self,root):
-        self.initUI(root) 
-
-    def initUI(self,root):
-        print ("hello")
-
-    LabelFrameFG="purple"
-    LabelFrameBG="SNOW"
-
-    note = Notebook(root)
-    tab1 = Frame(note)
-    tab6 = Frame(note)
-
-
-def main():
-    root = Tk()
-    root.resizable(0,0)
-    root.state('zoomed')
-    GUI=GUIDesign(root)
-    root.mainloop()
-
-if __name__ == '__main__':
-    main()
-
-print('\n\nThis script allows plotting EPMA quantitative maps and do calculations with the data within.\nPlease be aware that this script is designed for the output file obtained with CalcImage (J. Donovan).\nThe input file for this script is a .xslx file, so you should convert .DAT to .xslx before\nat least the following column names (case and character sensitive):\nX,Y,NX,NY,NXY,SiO2 WT%,TiO2 WT%,Al2O3 WT%,FeO WT%,MnO WT%,MgO WT%,CaO WT%,Na2O WT%,K2O WT%,P2O5 WT%,Total\nX and Y: x-y coordinates EPMA position, NX and NY: pixel number in x-y (matrix)\nNXY: consecutive pixel number, all the rest values are weight percents including the total.\nUnfortunatelly this script does not work with more oxides or halogens at the moment, but can be easily\nmodified to do so by the user. Just keep in mind to also modify the external functions that are being used.\n')
-
 #Importing libraries a$nd changing plotting parameters
 
 import numpy as np #math functions as pi,e,random,arrays, etc 
@@ -54,6 +12,8 @@ plt.rcParams['figure.dpi']   = 200 #increases the quality of images
 import mpltern #to plot ternary diagrams
 from matplotlib_scalebar.scalebar import ScaleBar #to add a scale bar to the maps
 from functionsmapping import to_anhydrous, to_mol, to_cat, norm_calc, add_fe2o3 #importing external functions
+
+print('\n\nThis script allows plotting EPMA quantitative maps and do calculations with the data within.\nPlease be aware that this script is designed for the output file obtained with CalcImage (J. Donovan).\nThe input file for this script is a .xslx file, so you should convert .DAT to .xslx before\nat least the following column names (case and character sensitive):\nX,Y,NX,NY,NXY,SiO2 WT%,TiO2 WT%,Al2O3 WT%,FeO WT%,MnO WT%,MgO WT%,CaO WT%,Na2O WT%,K2O WT%,P2O5 WT%,Total\nX and Y: x-y coordinates EPMA position, NX and NY: pixel number in x-y (matrix)\nNXY: consecutive pixel number, all the rest values are weight percents including the total.\nUnfortunatelly this script does not work with more oxides or halogens at the moment, but can be easily\nmodified to do so by the user. Just keep in mind to also modify the external functions that are being used.\n')
 
 #Reading excel file with major and trace elements in glasses
 name = input('Please type the path where the data file is located e.g. Documents/Python/Mydata.xlsx')
