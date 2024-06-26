88bbcea0c6d22964e9c60e6c9e906d6742a7511d
Edgar Alejandro Cortes-Calderon <eacortesc@gmail.com>
2024-06-26 15:31:39 +0000

    some adjustments

--- epMapy.py
+++ epMapy.py
@@ -1,3 +1,43 @@
+from tkinter import *
+import tkinter as tk
+from tkinter.ttk import *
+from tkinter import LabelFrame, Label, Tk#, Canvas
+from tkinter.ttk import Notebook
+import tkinter.messagebox
+import time
+import os
+
+
+import warnings
+warnings.filterwarnings('ignore')
+
+root = tk.Tk()
+
+class GUIDesign():
+    def __init__(self,root):
+        self.initUI(root) 
+
+    def initUI(self,root):
+        print ("hello")
+
+    LabelFrameFG="purple"
+    LabelFrameBG="SNOW"
+
+    note = Notebook(root)
+    tab1 = Frame(note)
+    tab6 = Frame(note)
+
+
+def main():
+    root = Tk()
+    root.resizable(0,0)
+    root.state('zoomed')
+    GUI=GUIDesign(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
+
 print('\n\nThis script allows plotting EPMA quantitative maps and do calculations with the data within.\nPlease be aware that this script is designed for the output file obtained with CalcImage (J. Donovan).\nThe input file for this script is a .xslx file, so you should convert .DAT to .xslx before\nat least the following column names (case and character sensitive):\nX,Y,NX,NY,NXY,SiO2 WT%,TiO2 WT%,Al2O3 WT%,FeO WT%,MnO WT%,MgO WT%,CaO WT%,Na2O WT%,K2O WT%,P2O5 WT%,Total\nX and Y: x-y coordinates EPMA position, NX and NY: pixel number in x-y (matrix)\nNXY: consecutive pixel number, all the rest values are weight percents including the total.\nUnfortunatelly this script does not work with more oxides or halogens at the moment, but can be easily\nmodified to do so by the user. Just keep in mind to also modify the external functions that are being used.\n')
 
 #Importing libraries a$nd changing plotting parameters
